d69aab6a253a947499bd0b34c60d8ad921ec8238
NakaoHina <menkuiudonkui@gmail.com>
2019-03-02 11:11:21 +0900

    create inputJpy and cancelButton

--- ticket-machine/src/store.js
+++ ticket-machine/src/store.js
@@ -23,6 +23,7 @@ export default new Vuex.Store({
     inputJpy10: 0, 
   },
   mutations: {
+    // inputJpy10,000
     inputJpy10000 (state) {
       if(state.jpy10000 === 0) {
         return 
@@ -30,9 +31,84 @@ export default new Vuex.Store({
       state.inputJpy10000 += 1
       state.jpy10000 -= 1
     },
+
+    // inputJpy5,000
+    inputJpy5000 (state) {
+      if(state.jpy5000 === 0) {
+        return 
+      }
+      state.inputJpy5000 += 1
+      state.jpy5000 -= 1
+    },
+
+    // inputJpy1,000
+    inputJpy1000 (state) {
+      if(state.jpy1000 === 0) {
+        return 
+      }
+      state.inputJpy1000 += 1
+      state.jpy1000 -= 1
+    },
+
+    // inputJpy500
+    inputJpy500 (state) {
+      if(state.jpy500 === 0) {
+        return 
+      }
+      state.inputJpy500 += 1
+      state.jpy500 -= 1
+    },
+
+    // inputJpy100
+    inputJpy100 (state) {
+      if(state.jpy100 === 0) {
+        return 
+      }
+      state.inputJpy100 += 1
+      state.jpy100 -= 1
+    },
+
+    // inputJpy50
+    inputJpy50 (state) {
+      if(state.jpy50 === 0) {
+        return 
+      }
+      state.inputJpy50 += 1
+      state.jpy50 -= 1
+    },
+
+    // inputJpy10
+    inputJpy10 (state) {
+      if(state.jpy10 === 0) {
+        return 
+      }
+      state.inputJpy10 += 1
+      state.jpy10 -= 1
+    },
+    
     inputCancel(state){
+      // Jpy10,000
       state.jpy10000 += state.inputJpy10000
       state.inputJpy10000 = 0
+      // Jpy5,000
+      state.jpy5000 += state.inputJpy5000
+      state.inputJpy5000 = 0
+      // Jpy1,000
+      state.jpy1000 += state.inputJpy1000
+      state.inputJpy1000 = 0
+      // Jpy500
+      state.jpy500 += state.inputJpy500
+      state.inputJpy500 = 0
+      // Jpy100
+      state.jpy100 += state.inputJpy100
+      state.inputJpy100 = 0
+      // Jpy50
+      state.jpy50 += state.inputJpy50
+      state.inputJpy50 = 0
+      // Jpy10
+      state.jpy10 += state.inputJpy10
+      state.inputJpy10 = 0
+
     }
   },
 })
